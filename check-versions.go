baf174873025b74cd87ea99fb07107e364b58448
Roman Volosatovs <rvolosatovs@riseup.net>
2017-09-26 11:48:32 +0200

    check-versions: add new repos, allow specifying a token

--- check-versions.go
+++ check-versions.go
@@ -4,29 +4,62 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"text/tabwriter"
+
+	"golang.org/x/oauth2"
 
 	"github.com/google/go-github/github"
 )
 
 func main() {
+	token := flag.String("token", "", "Github token to use")
+	flag.Parse()
+
+	ctx := context.Background()
+
+	var tc *http.Client
+	if token != nil && *token != "" {
+		ts := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: *token},
+		)
+		tc = oauth2.NewClient(ctx, ts)
+	}
+	cl := github.NewClient(tc)
+
 	w := tabwriter.NewWriter(os.Stdout, 0, 8, 0, '\t', 0)
-	fmt.Fprintln(w, "Repository", "\tVersion",  "\tRelease page")
+	fmt.Fprintln(w, "Repository", "\tVersion", "\tRelease page")
 	for owner, repos := range map[string][]string{
 		"grpc":             {"grpc", "grpc-java", "grpc-swift"},
 		"grpc-ecosystem":   {"grpc-gateway"},
 		"protobuf-c":       {"protobuf-c"},
+		"ckaznocha":        {"protoc-gen-lint"},
+		"pseudomuto":       {"protoc-gen-doc"},
 		"TheThingsNetwork": {"ttn"},
+		"Masterminds":      {"glide"},
+		"stepancheg":       {"rust-protobuf", "grpc-rust"},
 	} {
 		for _, repo := range repos {
-			rel, _, err := github.NewClient(nil).Repositories.GetLatestRelease(context.Background(), owner, repo)
+			rel, _, err := cl.Repositories.GetLatestRelease(context.Background(), owner, repo)
 			if err != nil {
+				if err, ok := err.(*github.ErrorResponse); ok && err.Response.StatusCode == http.StatusNotFound {
+					tags, _, err := cl.Repositories.ListTags(context.Background(), owner, repo, nil)
+					if err != nil {
+						log.Fatal(err)
+					}
+					if len(tags) == 0 {
+						log.Fatal("%s/%s repo has 0 tags", owner, repo)
+					}
+					fmt.Fprintf(w, "%s/%s\t%s\t%s\n", owner, repo, *tags[0].Name, "n/a")
+					continue
+				}
 				log.Fatal(err)
 			}
-			fmt.Fprintf(w, "%s/%s\t%s\t%s\n", owner, repo, *rel.TagName,  *rel.HTMLURL)
+			fmt.Fprintf(w, "%s/%s\t%s\t%s\n", owner, repo, *rel.TagName, *rel.HTMLURL)
 		}
 	}
 	if err := w.Flush(); err != nil {
