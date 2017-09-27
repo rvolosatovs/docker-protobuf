a43f47bc18b3614591686f1eae5412505ce8f2fb
Roman Volosatovs <rvolosatovs@riseup.net>
2017-09-27 19:29:32 +0200

    travis: fix tagged build

--- travis-push.sh
+++ travis-push.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-set -ev
+set -xe
 
-if ! [ -n ${TRAVIS_TAG} ]; then
+if [ ${TRAVIS_TAG} ]; then
     docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
     TAG=${TRAVIS_TAG#"v"}
-    for name in ${IMAGE_NAME}:latest ${IMAGE_NAME}:${TAG} ${IMAGE_NAME}:${TAG:0:1} ${IMAGE_NAME}:${TAG:0:3} ${IMAGE_NAME}:${TAG:0:5}; do
-        docker tag ${name} && docker push ${name}
+    for name in ${IMAGE_NAME}:${TAG} ${IMAGE_NAME}:${TAG:0:1} ${IMAGE_NAME}:${TAG:0:3} ${IMAGE_NAME}:${TAG:0:5}; do
+        docker tag ${IMAGE_NAME}:latest ${name} && docker push ${name}
     done
 fi
