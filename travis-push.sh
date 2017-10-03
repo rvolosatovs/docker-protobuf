cda0688d06d9988becdefc29a675ec584d9ae736
Roman Volosatovs <rvolosatovs@riseup.net>
2017-10-03 12:19:13 +0200

    travis: tag v3 builds as latest

--- travis-push.sh
+++ travis-push.sh
@@ -4,6 +4,7 @@ set -xe
 if [ ${TRAVIS_TAG} ]; then
     docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
     TAG=${TRAVIS_TAG#"v"}
+    [[ ${TAG} == 3* ]] && docker push ${IMAGE_NAME}:latest
     for name in ${IMAGE_NAME}:${TAG} ${IMAGE_NAME}:${TAG:0:1} ${IMAGE_NAME}:${TAG:0:3} ${IMAGE_NAME}:${TAG:0:5}; do
         docker tag ${IMAGE_NAME}:latest ${name} && docker push ${name}
     done
