538bc18aa5dd04402a544ca69dbac13971980594
Roman Volosatovs <rvolosatovs@riseup.net>
2018-10-22 22:45:01 +0200

    travis-push: Fix tagging

--- travis-push.sh
+++ travis-push.sh
@@ -5,7 +5,7 @@ if [ ${TRAVIS_TAG} ]; then
     docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
     TAG=${TRAVIS_TAG#"v"}
     [[ ${TAG} == 3* ]] && docker push ${IMAGE_NAME}:latest
-    for name in ${IMAGE_NAME}:${TAG} ${IMAGE_NAME}:${TAG:0:1} ${IMAGE_NAME}:${TAG:0:3} ${IMAGE_NAME}:${TAG:0:5}; do
+    for name in ${IMAGE_NAME}:${TAG} ${IMAGE_NAME}:${TAG%-*} ${IMAGE_NAME}:${TAG%.*-*} ${IMAGE_NAME}:${TAG%.*.*-*}; do
         docker tag ${IMAGE_NAME}:latest ${name} && docker push ${name}
     done
 fi
