7ab103b6ec9bd95d8ae2aa5625ff88943c4c4c24
Roman Volosatovs <roman@thethingsnetwork.org>
2019-03-18 13:31:01 +0100

    travis-push.sh: Only push the full version on pre-releases

--- travis-push.sh
+++ travis-push.sh
@@ -4,8 +4,12 @@ set -xe
 if [ ${TRAVIS_TAG} ]; then
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     TAG=${TRAVIS_TAG#"v"}
-    [[ ${TAG} == 3* ]] && docker push ${IMAGE_NAME}:latest
-    for name in ${IMAGE_NAME}:${TAG} ${IMAGE_NAME}:${TAG%-*} ${IMAGE_NAME}:${TAG%.*-*} ${IMAGE_NAME}:${TAG%.*.*-*}; do
-        docker tag ${IMAGE_NAME}:latest ${name} && docker push ${name}
-    done
+    if [[ ${TAG} =~ 3\.[0-9]+\.[0-9]+$ ]]; then 
+        docker push ${IMAGE_NAME}:latest
+        for name in ${IMAGE_NAME}:${TAG} ${IMAGE_NAME}:${TAG%.*} ${IMAGE_NAME}:${TAG%.*.*}; do
+            docker tag ${IMAGE_NAME}:latest ${name} && docker push ${name}
+        done
+    else
+        docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${TAG} && docker push ${IMAGE_NAME}:${TAG}
+    fi
 fi
