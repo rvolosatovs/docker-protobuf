0bdbb05af6c6b0ec8c2d0d993d6afc7384101127
Roman Volosatovs <roman@thethingsnetwork.org>
2019-05-17 18:05:26 +0200

    .travis.yml,travis-push.sh: Push development images on each build

--- travis-push.sh
+++ travis-push.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 set -xe
 
-if [ ${TRAVIS_TAG} ]; then
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+function tag_and_push {
+    docker tag "${IMAGE_NAME}:latest" "${IMAGE_NAME}:${1}" && docker push "${IMAGE_NAME}:${1}"
+}
+    
+
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+if [ "${TRAVIS_TAG}" ]; then
     TAG=${TRAVIS_TAG#"v"}
     if [[ ${TAG} =~ 3\.[0-9]+\.[0-9]+$ ]]; then 
-        docker push ${IMAGE_NAME}:latest
-        for name in ${IMAGE_NAME}:${TAG} ${IMAGE_NAME}:${TAG%.*} ${IMAGE_NAME}:${TAG%.*.*}; do
-            docker tag ${IMAGE_NAME}:latest ${name} && docker push ${name}
-        done
+        docker push "${IMAGE_NAME}:latest"
+        tag_and_push "${TAG}"
+        tag_and_push "${TAG%.*}"
+        tag_and_push "${TAG%.*.*}"; 
     else
-        docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${TAG} && docker push ${IMAGE_NAME}:${TAG}
+        tag_and_push "${TAG}"
     fi
+elif [ "${TRAVIS_BRANCH}" = "master" ]; then
+    tag_and_push "nightly"
+else 
+    tag_and_push "development"
 fi
