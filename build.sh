dd7978e89af8d3734e259bb5b380bb3ef21b9544
Mohammed Al Sahaf <msaa1990@gmail.com>
2021-08-20 12:33:50 +0300

    build.sh: Update dependencies
    The update of dart invovles a change of the repo from dart-lang org to google

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 docker build \
---build-arg ALPINE_VERSION="${ALPINE_VERSION:-"3.12"}" \
---build-arg DART_PROTOBUF_VERSION="${DART_PROTOBUF_VERSION:-"1.1.0"}" \
---build-arg DART_VERSION="${DART_VERSION:-"2.10.4"}" \
+--build-arg ALPINE_VERSION="${ALPINE_VERSION:-"3.14"}" \
+--build-arg DART_PROTOBUF_VERSION="${DART_PROTOBUF_VERSION:-"2.0.0"}" \
+--build-arg DART_VERSION="${DART_VERSION:-"2.13.4"}" \
 --build-arg GOOGLE_API_VERSION="d9b32e92fa57c37e5af0dc03badfe741170c5849" \
---build-arg GO_VERSION="${GO_VERSION:-"1.15.6"}" \
+--build-arg GO_VERSION="${GO_VERSION:-"1.17.0"}" \
 --build-arg GRPC_GATEWAY_VERSION="${GRPC_GATEWAY_VERSION:-"2.3.0"}" \
 --build-arg GRPC_JAVA_VERSION="${GRPC_JAVA_VERSION:-"1.36.0"}" \
 --build-arg GRPC_RUST_VERSION="${GRPC_RUST_VERSION:-"0.8.2"}" \
 --build-arg GRPC_SWIFT_VERSION="${GRPC_SWIFT_VERSION:-"1.0.0"}" \
 --build-arg GRPC_VERSION="${GRPC_VERSION:-"1.36.4"}" \
 --build-arg GRPC_WEB_VERSION="${GRPC_WEB_VERSION:-"1.2.1"}" \
---build-arg NODE_VERSION="${NODE_VERSION:-"14.15.4"}" \
+--build-arg NODE_VERSION="${NODE_VERSION:-"14.17.5"}" \
 --build-arg PROTOBUF_C_VERSION="${PROTOBUF_C_VERSION:-"1.3.3"}" \
 --build-arg PROTOC_GEN_DOC_VERSION="${PROTOC_GEN_DOC_VERSION:-"1.4.1"}" \
 --build-arg PROTOC_GEN_FIELDMASK_VERSION="${PROTOC_GEN_FIELDMASK_VERSION:-"0.4.5"}" \
