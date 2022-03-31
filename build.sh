4bf5b19b7df2da60204dd6529761bef399539b67
Roman Volosatovs <rvolosatovs@riseup.net>
2022-03-31 11:39:48 +0200

    refactor: avoid specifying `--platform` in `build.sh`
    Build only for default platform by default, if a custom platform is
    required, users should pass `--platform` flag to the script.

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 set -a
 
 . ./deps.list
-docker buildx build --platform linux/amd64,linux/arm64 \
+docker buildx build \
     $(for v in $(cut -d '=' -f 1 < deps.list); do printf "%s " "--build-arg $v=$(printenv ${v})"; done) \
     ${@} .
