4315695188b5b56b71798d384316e3e85a9fa4cd
Roman Volosatovs <rvolosatovs@riseup.net>
2025-11-12 20:17:26 +0100

    chore: remove stale `build.sh`
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-set -a
-
-. ./deps.list
-docker buildx build \
-    $(for v in $(cut -d '=' -f 1 < deps.list); do printf "%s " "--build-arg $v=$(printenv ${v})"; done) \
-    ${@} .
